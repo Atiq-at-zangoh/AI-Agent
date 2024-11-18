73b6e57b2c91bb203253b78fe22e47c6ea720a2f
Atiq-at-zangoh <atiq@zangoh.com>
2024-11-18 17:24:54 +0530

    Add Tower of Hanoi code in Python

---
+++ tower_of_hanoi.py
@@ -0,0 +1,29 @@
+# tarke of hoanio
+
+# Tower of Haoni code in Python
+
+# See the data needs for tour time.
+#
+
+# Constant:
+    data = 1, bond = 2., remove = 3.
+
+# Select:
+    if datas = 11:
+
+    home = tour_footer (HLOBAL, 1, datas, remove)
+
+    remove = tour_footer (LOBGLL - datas, remove)
+
+    rel = tour_footer + 1, logged = datas - remove
+    rel = tour_footer + 1, logged = datas - remove
+    rel = tour_footer + 1, logged = datas - remove
+    rel = tour_footer + 1, logged = datas - remove
+    rel = tour_footer + 1, logged = datas - remove
+    rel = tour_footer + 1, logged = datas - remove
+    rel = tour_footer + 1, logged = datas - remove
+    rel = tour_footer + 1, logged = datas - remove
+    rel = tour_footer + 1, logged = datas - remove
+
+    def towr_footer:
+        logged = datas - remove - tour_footer * 1, logged * 23, 1.
